ab8c09689fe4e15adb54c6de0d81e3190332a3bb
Bastian Hoyer <dafire@gmail.com>
2018-05-15 13:20:07 +0200

    code climate test

--- config/run-tests.sh
+++ config/run-tests.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-./manage.py test
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > "$/usr/local/bin/cc-test-reporter" && chmod +x "/usr/local/bin/cc-test-reporter"
+
+cc-test-reporter before-build
+
+coverage run --source='.' ./manage.py test
+
+cc-test-reporter after-build --exit-code $?
 
 codecov
