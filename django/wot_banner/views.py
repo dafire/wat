ac05f985853455101d07698f6dac542b2d69db30
Bastian Hoyer <bastian.hoyer@sinnerschrader.com>
2018-06-14 18:24:03 +0200

    update

--- django/wot_banner/views.py
+++ django/wot_banner/views.py
@@ -6,31 +6,33 @@ from django.http import HttpResponse
 PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def draw_caption(img, text, top=False):
-    draw = ImageDraw.Draw(img)
-    # Find a suitable font size to fill the entire width:
-    w = img.size[0]
-    s = 100
-    while w >= (img.size[0] - 20):
-        font = ImageFont.truetype(os.path.join(PATH, "Subway-Black.ttf"), s)
-        w, h = draw.textsize(text, font=font)
-        s -= 1
-        if s <= 12: break
-    # Draw the text multiple times in black to get the outline:
-    for x in range(-3, 4):
-        for y in range(-3, 4):
-            draw_y = y if top else img.size[1] - h + y
-            draw.text((10 + x, draw_y), text, font=font, fill='black')
-    # Draw the text once more in white:
-    draw_y = 0 if top else img.size[1] - h
-    draw.text((10, draw_y), text, font=font, fill='white')
+class Banner():
+    def __init__(self):
+        self.image = Image.open(os.path.join(PATH, "banner.png"))
+
+    def draw_caption(self, text, size=20, outline=2, x_offset=2, y_offset=2):
+        draw = ImageDraw.Draw(self.image)
+
+        font = ImageFont.truetype(os.path.join(PATH, "Subway-Black.ttf"), size)
+
+        # Draw the text multiple times in black to get the outline:
+        for x in range(1 - outline, outline):
+            for y in range(1 - outline, outline):
+                draw.text((x + x_offset, y + y_offset), text, font=font, fill='black')
+        # Draw the text once more in white:
+
+        draw.text((x_offset, y_offset), text, font=font, fill='white')
+        size = draw.textsize(text, font=font)
+        return size[0] + x_offset, size[1] + y_offset
+
+    def response(self):
+        response = HttpResponse(content_type="image/png")
+        self.image.save(response, "PNG")
+        return response
 
 
 def banner(request):
-    image = Image.open(os.path.join(PATH, "banner.png"))
-
-    draw_caption(image, "Generol")
-
-    response = HttpResponse(content_type="image/png")
-    image.save(response, "PNG")
-    return response
+    img = Banner()
+    x, y = img.draw_caption("Generol ")
+    print(img.draw_caption("[aim-j]", x_offset=x))
+    return img.response()
