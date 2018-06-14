4700952494e08da772e424e1a5b17f0e871742e6
Bastian Hoyer <dafire@gmail.com>
2018-06-14 23:42:12 +0200

    banner

--- django/wot_banner/views.py
+++ django/wot_banner/views.py
@@ -1,7 +1,11 @@
-import os
+from collections import defaultdict
 
+import os
 from PIL import Image, ImageDraw, ImageFont
 from django.http import HttpResponse
+from django.views.decorators.cache import cache_control
+
+from wot_web_wtr.models import WebWtrRating
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,18 +14,20 @@ class Banner():
     def __init__(self):
         self.image = Image.open(os.path.join(PATH, "banner.png"))
 
-    def draw_caption(self, text, size=20, outline=2, x_offset=2, y_offset=2):
+    def draw_caption(self, text, headline=False, color='white', size=None, outline=3, x_offset=2, y_offset=2):
         draw = ImageDraw.Draw(self.image)
-
-        font = ImageFont.truetype(os.path.join(PATH, "Subway-Black.ttf"), size)
-
+        text = str(text)
+        if headline:
+            font = ImageFont.truetype(os.path.join(PATH, "Subway-Black.ttf"), size or 20)
+        else:
+            font = ImageFont.truetype(os.path.join(PATH, "VeraMono.ttf"), size or 14)
         # Draw the text multiple times in black to get the outline:
         for x in range(1 - outline, outline):
             for y in range(1 - outline, outline):
                 draw.text((x + x_offset, y + y_offset), text, font=font, fill='black')
         # Draw the text once more in white:
 
-        draw.text((x_offset, y_offset), text, font=font, fill='white')
+        draw.text((x_offset, y_offset), text, font=font, fill=color)
         size = draw.textsize(text, font=font)
         return size[0] + x_offset, size[1] + y_offset
 
@@ -31,8 +37,63 @@ class Banner():
         return response
 
 
+LABEL = {"day": "Gestern", "overall": "seit Dez.14"}
+
+OFFSET = 100
+WTR_X = OFFSET + 170
+WINRATE_X = OFFSET + 260
+GEFECHTE_X = OFFSET + 110
+
+
+@cache_control(max_age=7200)
 def banner(request):
     img = Banner()
-    x, y = img.draw_caption("Generol ")
-    print(img.draw_caption("[aim-j]", x_offset=x))
+    ratings = WebWtrRating.objects.values('date', 'time_slice', 'personal') \
+        .filter(time_slice__in=['day', 'overall', '2018-06']) \
+        .filter(account_id="500447063", tier_group=0) \
+        .distinct('time_slice') \
+        .order_by('time_slice', '-date') \
+        .values_list('date', 'time_slice', 'personal', named=True)
+
+    nickname = ""
+    clan = ""
+    rat = defaultdict(lambda: (0, 0, 0, 0, 0))
+    for r in ratings:
+        rat[r.time_slice] = (
+            r.personal.get("sbr"), r.personal.get("sbr_delta"), r.personal.get("battles_count"),
+            r.personal.get("win_rate"),
+            r.personal.get("win_rate_delta"))
+        nickname = r.personal.get("nickname", '')
+        clan = r.personal.get("clan_info", {}).get('tag', '')
+    x, y = img.draw_caption(nickname, headline=True)
+    if clan:
+        img.draw_caption("[%s]" % clan, x_offset=x + 5, headline=True)
+
+    line = y + 10
+    img.draw_caption("WTR", x_offset=WTR_X, y_offset=line)
+    img.draw_caption("WinRate", x_offset=WINRATE_X, y_offset=line)
+    img.draw_caption("Gef.", x_offset=GEFECHTE_X, y_offset=line)
+
+    for l in ['day', '2018-06', 'overall']:
+        line = line + 16
+        img.draw_caption(LABEL.get(l, l) + ":", y_offset=line)
+
+        x, _ = img.draw_caption(rat[l][2], x_offset=GEFECHTE_X, y_offset=line)
+
+        x, _ = img.draw_caption(rat[l][0], x_offset=WTR_X, y_offset=line)
+        if rat[l][1]:
+            if rat[l][1] > 0:
+                color = 'lightgreen'
+            else:
+                color = 'red'
+            img.draw_caption("({0:+})".format(rat[l][1]), color=color, x_offset=x + 1, y_offset=line)
+
+        x, _ = img.draw_caption(rat[l][3], x_offset=WINRATE_X, y_offset=line)
+        if rat[l][4]:
+            if rat[l][4] > 0:
+                color = 'lightgreen'
+            else:
+                color = 'red'
+            img.draw_caption("({0:+})".format(rat[l][4]), color=color, x_offset=x + 1, y_offset=line)
+
     return img.response()
