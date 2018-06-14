02f6c04e1e9634a0a4caa0f1d300c3a50c6ea63e
Bastian Hoyer <dafire@gmail.com>
2018-06-15 01:13:49 +0200

    test

--- django/wot_banner/views.py
+++ django/wot_banner/views.py
@@ -33,7 +33,7 @@ class Banner():
 
     def response(self):
         response = HttpResponse(content_type="image/jpeg")
-        self.image.save(response, "JPEG", quality=95)
+        self.image.save(response, "JPEG", quality=90, progression=True, optimize=True, dpi=(72, 72))
         return response
 
 
