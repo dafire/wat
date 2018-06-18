d5aa81cd201c0fadcba385498f2ff92a2770b8da
Bastian Hoyer <dafire@gmail.com>
2018-06-18 19:16:20 +0200

    show last run

--- django/wot_admin_tools/admin.py
+++ django/wot_admin_tools/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
-
 # Register your models here.
+from django_celery_beat.admin import PeriodicTaskAdmin
+
+PeriodicTaskAdmin.list_display = ('__str__', 'last_run_at', 'total_run_count', 'enabled')
